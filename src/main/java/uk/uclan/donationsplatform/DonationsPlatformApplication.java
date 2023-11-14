22eed37c4ff9973353eedeaa8ac3bfe5e19b6119
yreisinger <yreisinger@uclan.ac.uk>
2023-11-14 12:42:03 +0000

    Initial commit

---
+++ src/main/java/uk/uclan/donationsplatform/DonationsPlatformApplication.java
@@ -0,0 +1,14 @@
+package uk.uclan.donationsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonationsPlatformApplication.class, args);
+    }
+
+    
+}
---
+++ src/test/java/uk/uclan/donationsplatform/DonationsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.uclan.donationsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
